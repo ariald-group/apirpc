c033b443934283ab2ec975612cedae54e7f4a1b4
Vitaliy Kurshinov <vitaliy@kurshinov.com>
2019-05-07 09:23:00 +0300

    fix

--- ApiRpc/ApiRpcJsonParser.php
+++ ApiRpc/ApiRpcJsonParser.php
@@ -7,7 +7,7 @@
      * @author    Vitaliy Kurshinov
      * @copyright Ariald Group
      * @link      https://ariald.com
-     * @link      https://github.com/kurshinov/ApiRpc
+     * @link      https://github.com/ariald-group/apirpc
      *
      * @license   GNU General Public License v3.0
      * @license   https://github.com/ariald-group/apirpc/blob/master/LICENSE
